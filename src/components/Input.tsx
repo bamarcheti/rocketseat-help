9895a08e5f1fd6278a276f62692d413f497d6646
Barbara Marcheti Fiorin <bfiorin@PSG.LOCAL>
2022-07-25 17:25:37 -0400

    aula-03

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,21 +3,20 @@ import { Input as NativeBaseInput, IInputProps } from 'native-base';
 export function Input({ ...rest }: IInputProps) {
   return (
     <NativeBaseInput
-    bg="gray.700"
-    h={14}
-    size="md"
-    borderWidth={0}
-    fontSize="md"
-    fontFamily="body"
-    color="white"
-    w="full"
-    placeholderTextColor="gray.300"
-    _focus={{
+      bg="gray.700"
+      h={14}
+      size="md"
+      borderWidth={0}
+      fontSize="md"
+      fontFamily="body"
+      color="white"
+      placeholderTextColor="gray.300"
+      _focus={{
         borderWidth: 1,
         borderColor: "green.500",
         bg: "gray.700"
-    }}
-    {...rest}
+      }}
+      {...rest}
     />
   );
 }
